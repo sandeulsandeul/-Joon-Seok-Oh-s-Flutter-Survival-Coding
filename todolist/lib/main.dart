7ca85d841b911dc50a2189806c60bf54dfdec912
sandeulsandeul <62785166+sandeulsandeul@users.noreply.github.com>
2020-04-27 20:10:09 +0900

    변수 준비
    할 일 목록을 저장할 변수와 입력받은 할 일 문자열을 조작하는 컨트롤러를 준비한다.

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -22,13 +22,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+// TodoListPage 클래스
 class TodoListPage extends StatefulWidget {
   @override
   _TodoListPageState  createState() => _TodoListPageState();
 }
-
+// TodoListPage State클래스
 class _TodoListPageState extends State<TodoListPage>{
+  // 할 일 목록을 저장할 리스트
+  final _items = <Todo>[];
+  // 할 일 문자열 조작을 위한 커트롤러
+  var _todoController = TextEditingController();
+  @override
+  void dispose(){
+    _todoController.dispose(); // 사용이 끝나면 해제
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
